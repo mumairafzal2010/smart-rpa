25a4c052b5ebfe8d2f26f6ded44a36e53d879c17
Sarim Ahmed <sarim@1x-nat-vl931-172-30-189-54.wireless.umass.edu>
2018-10-14 00:19:04 -0400

    database commit

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -2,6 +2,10 @@ import { Component, OnInit } from '@angular/core';
 import { Rpa } from '../rpa';
 import { SMARTRPA } from '../smart-rpa';
 import { ListService } from '../list.service';
+import { Http, Response } from '@angular/http';
+import { Injectable } from '@angular/core';
+import {HttpParams, HttpClient} from "@angular/common/http";
+
 
 @Component({
   selector: 'app-list',
@@ -13,10 +17,24 @@ export class ListComponent implements OnInit {
   rpa = SMARTRPA;
   selectedRPA: Rpa;
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
 
   ngOnInit() {
+    this.getUser()
   }
+  getUser() {
+    const params = new HttpParams().set('id', "126");
+    return this.httpClient.get("https://api.appery.io/rest/1/apiexpress/api/MessageAPI-RPA/23?apiKey=a2e6d55a-6341-4be6-9e55-8f62d81cdd9d", {params}).subscribe(
+      data => {
+        console.log("PUT Request is successful ", data);
+    },
+    error => {
+      console.log(error);
+      
+    }
+    );
+  }
+
 onSelect(srpa: Rpa): void {
     this.selectedRPA = srpa;
   }
